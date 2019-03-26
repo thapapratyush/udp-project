a254722f69859f7496f75ee4ec590e03a0cddaa8
thapapratyush <thapa.pratyush10@gmail.com>
2019-03-26 13:47:24 -0700

    Add basic structure to the client including getting the arguments creating a socket and binding to the socket. Handling failures by logging and exiting as well

--- client.c
+++ client.c
@@ -22,6 +22,31 @@ int main(int argc, char *argv[]) {
     char          buffer[MAX_LINE];       // character buffer          
     char*         ip_address;             //  Holds remote IP address   
     char*         remote_port;            //  Holds remote port         
-    char*     	  endptr;                 //  for strtol() 
+    char*     	  endptr;                 //  for strtol()
+
+    if (argc != 3){
+        fprintf(stderr, "ERROR! Invalid number of arguments.\n");
+        exit();
+    } else {
+        port = argv[1];
+        ip_address = argv[2];
+        printf("Port number %d and IP address received %s", port, ip_address);
+    }
+
+
+	if((soc = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+        fprintf(stderr, "ERROR! Unable to create socket.\n");
+        exit(EXIT_FAILURE);
+    }
+
+	if(bind(soc, (struct sockaddr*) &servaddr, sizeof(servaddr)) < 0 ){
+		fprintf(stderr, "ERROR! Could not bind name to the given socket.\n");
+        exit(EXIT_FAILURE);
+	}
+
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(port);
+	servaddr.sin_addr.s_addr = INADDR_ANY;
 
 }
